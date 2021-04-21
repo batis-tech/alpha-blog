f8b002a831f3499a3d187d0f1426cc7d53a23faf
hasan <hasanbatis440@gmail.com>
2021-04-21 13:17:12 +0300

    last update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
-  
+  root 'pages#home'
+get 'about', to: 'pages#about'
+resources :articles
+get 'signup', to: 'users#new'
+resources :users, except: [:new]
+end
   #same as the one below is just short cut
   # resources :articles, only:[:show, :index, :new, :create, :edit, :update, :destroy]
-end
