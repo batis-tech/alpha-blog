e12c3479fbda1b32f4ee065dfb37a3cc018c7453
hasan <hasanbatis440@gmail.com>
2021-04-21 14:28:04 +0300

    create user to the system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    @user = User.new(user_perams)
+    if @user.save
+      flash[:notice] = "Welcome to  #{@user.username},you have successfuly signed up"
+      redirect_to articles_path
 
-end 
+  else
+    render 'new'
+  end
+  end
+
+  private
+  def user_perams
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
