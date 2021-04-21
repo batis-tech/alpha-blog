a466effb8e9f4c3d218239e39eaa82002325b1f3
hasan <hasanbatis440@gmail.com>
2021-04-21 15:00:27 +0300

    edit user data functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+   @user = User.find(params[:id])
+   if @user.update(user_perams)
+     flash[:notice] = "Your account has been updated successfuly #{@user.username}"
+     redirect_to articles_path
+  else
+    render 'edit'
+  end
+  end
   def create
     @user = User.new(user_perams)
     if @user.save
